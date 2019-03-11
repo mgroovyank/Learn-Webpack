0c0b8d67c0ab199208f7b966c03b7fd8aacedd1b
Mayank Chhipa <mayanksamariya99@gmail.com>
2019-03-11 09:31:28 +0000

    installed webpack

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
-
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
