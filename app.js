7698d75893ef4113777ff4c1d7687b9b08830d65
Mayank Chhipa <mayanksamariya99@gmail.com>
2019-03-11 08:26:50 +0000

    initial file setup

---
+++ app.js
@@ -0,0 +1,15 @@
+var express=require("express");
+var app=express();
+var bodyParser=require("body-parser");
+
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server ready to go!");
+});
