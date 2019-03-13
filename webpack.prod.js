dbb2bc00b680b18473c6cc26c6c1e28fbfe4b603
Mayank Chhipa <mayanksamariya99@gmail.com>
2019-03-13 04:06:30 +0000

    add html-loader file-loader and clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path=require("path");//no need to install path(built in node)
 const common=require("./webpack.common");
 const merge=require("webpack-merge");
+const cleanWebpackPlugin=require("clean-webpack-plugin");
 module.exports=merge(common,{
     mode:"production",
     //devtool:"none"(put only when you want to remove eval statements from main.js)
     output:{
         filename:"main.[contentHash].js",
         path:path.resolve(__dirname,"dist")
-    }
+    },
+    plugins:[new cleanWebpackPlugin()]
 });
