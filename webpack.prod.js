1feea2f48f68b09f3cd156a18d57a47cf8c329ff
Mayank Chhipa <mayanksamariya99@gmail.com>
2019-03-13 04:35:19 +0000

    last commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path=require("path");//no need to install path(built in node)
 const common=require("./webpack.common");
 const merge=require("webpack-merge");
 const cleanWebpackPlugin=require("clean-webpack-plugin");
+const MiniCssExtractPlugin=require("mini-css-extract-plugin");
 module.exports=merge(common,{
     mode:"production",
     //devtool:"none"(put only when you want to remove eval statements from main.js)
@@ -9,5 +10,19 @@ module.exports=merge(common,{
         filename:"[name].[contentHash].bundle.js",
         path:path.resolve(__dirname,"dist")
     },
-    plugins:[new cleanWebpackPlugin()]
+    plugins:[new MiniCssExtractPlugin({filename:"[name].[contentHash].css"}),
+    new cleanWebpackPlugin()],
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                    ]
+            }
+            ]
+    }
+    
 });
